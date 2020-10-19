88512b17e77595f468702219bab38218d35b27a6
Homero Oliveira <homero_oliveira@icloud.com>
2020-10-19 06:59:27 -0300

    Update swift-argument-parser to 0.3.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,15 +12,18 @@ let package = Package(
         .executable(name: "bump", targets: ["Bump"])
     ],
     dependencies: [
-        .package(url: "https://github.com/tuist/xcodeproj.git",
+        .package(name: "XcodeProj",
+                 url: "https://github.com/tuist/xcodeproj.git",
                  .upToNextMajor(from: "7.10.0")),
-        .package(url: "https://github.com/apple/swift-argument-parser",
-                 from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git",
+                 .upToNextMajor(from: "0.3.1")),
     ],
     targets: [
         .target(
             name: "Bump",
-            dependencies: ["BumpCore", "ArgumentParser", "XcodeProjWrapper"]),
+            dependencies: ["BumpCore",
+                           .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                           "XcodeProjWrapper"]),
         .target(
             name: "BumpCore",
             dependencies: ["XcodeProjWrapper", "SwiftExtensions"]),
--- Sources/Bump/BumpCommand.swift
+++ Sources/Bump/BumpCommand.swift
@@ -19,14 +19,14 @@ struct BumpCommand: ParsableCommand {
     @Option(name: .shortAndLong, help: "Increment mode to bump version or build number. Either 'major', 'minor', 'patch', or 'build'.")
     var mode: IncrementMode
     
-    @Option(name: .shortAndLong, default: fileManager.currentDirectoryPath, help: "The path of .xcodeproj file or directory. Default value is the current directory.")
-    var path: String
+    @Option(name: .shortAndLong, help: "The path of .xcodeproj file or directory. Default value is the current directory.")
+    var path = fileManager.currentDirectoryPath
     
     @Flag(name: .shortAndLong, help: "Show all the targets")
-    var verbose: Bool
+    var verbose = false
     
     @Flag(name: .shortAndLong, help: "Get the version of the first target and set it to the rest of the targets.")
-    var useSameVersion: Bool
+    var useSameVersion = false
     
     mutating func validate() throws {
         guard !bundleIdentifiers.isEmpty else {
@@ -34,7 +34,8 @@ struct BumpCommand: ParsableCommand {
         }
         
         if case .versionString(let version) = mode {
-            let hasValidFormat = version.range(of: #"\d+\.\d+\.\d+(\.\d+)?"#, options: .regularExpression) != nil
+            let versionPattern = #"\d+\.\d+\.\d+(\.\d+)?"#
+            let hasValidFormat = version.range(of: versionPattern, options: .regularExpression) != nil
             
             guard hasValidFormat else {
                 throw ValidationError("Invalid format, the version must only have numbers and have two dots or three dots. Example of versions: `1.0.0` or `1.0.0.1`.")
