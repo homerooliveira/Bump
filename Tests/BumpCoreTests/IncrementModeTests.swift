d672d9c510ada342c4f3c374f24cf375bf219cf9
Homero Oliveira <homero_oliveira@icloud.com>
2020-11-04 03:37:49 -0300

    Add tests to IncrementMode

---
+++ Tests/BumpCoreTests/IncrementModeTests.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Homero Oliveira on 04/11/20.
+//
+
+import Foundation
+import XCTest
+
+@testable import BumpCore
+
+final class IncrementModeTests: XCTestCase {
+    
+    func testRawValue() throws {
+        let expectedValues = ["major", "minor", "patch", "build", "1.0.0"]
+        
+        let modes: [IncrementMode] = [.major, .minor, .patch, .build, .versionString("1.0.0")]
+        
+        XCTAssertEqual(expectedValues, modes.map { $0.rawValue })
+    }
+}
