251ce879b5d88a430c094654ade0af946bd3c517
Homero Oliveira <homero_oliveira@icloud.com>
2020-11-14 00:05:53 -0300

    Add more tests

--- Tests/BumpCoreTests/BumpCoreTests.swift
+++ Tests/BumpCoreTests/BumpCoreTests.swift
@@ -227,4 +227,63 @@ final class BumpCoreTests: XCTestCase {
         XCTAssertEqual(logs.count, 1)
         XCTAssertTrue(xcodeProj.saveChangesCalled)
     }
+    
+    func testBumpOutputVerboseWhenInPlaceIsTrue() throws {
+        var logs: [String] = []
+        let xcodeProj = XcodeProjWrapperMock.mock
+        let bump = try Bump(
+            xcodeProj: xcodeProj,
+            bundleIdentifiers: ["com.test"],
+            log: { logs.append($0) },
+            isVerbose: true,
+            inPlace: false
+        )
+        
+        try bump.bump(flag: .minor)
+        
+        XCTAssertEqual(logs, ["Test2 1 -> 1.1.0.1"])
+        XCTAssertEqual(logs.count, 1)
+        XCTAssertFalse(xcodeProj.saveChangesCalled)
+    }
+    
+    func testBumpOutputVersionWithTwoBuildConfigs() throws {
+        let xcodeProj = XcodeProjWrapperMock(
+            targets: [
+                TargetMock(
+                    name: "Test1",
+                    buildConfigurations: [
+                        BuildConfigurationMock(
+                            bundleIdentifier: "test",
+                            buildNumber: "1",
+                            version: "1.0.0.1"
+                        ),
+                        BuildConfigurationMock(
+                            bundleIdentifier: "test",
+                            buildNumber: "1",
+                            version: "1.0.0.1"
+                        )
+                    ]
+                )
+            ]
+        )
+        var logs: [String] = []
+        
+        let bump = try Bump(
+            xcodeProj: xcodeProj,
+            bundleIdentifiers: ["test"],
+            log: { string in
+                logs.append(string)
+            },
+            isVerbose: true
+        )
+        
+        try bump.bump(flag: .build)
+        
+        XCTAssertEqual(
+            logs,
+            ["Test1 1 -> 1.0.0.2"]
+        )
+        XCTAssertEqual(logs.count, 1)
+        XCTAssertTrue(xcodeProj.saveChangesCalled)
+    }
 }
