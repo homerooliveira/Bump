74acc4f37c27a6c4585463ccf9e27cf701cb091b
Homero Oliveira <homero_oliveira@icloud.com>
2024-01-21 21:19:52 -0300

    Fix case sensitivity issue in URL+isXcodeProj.swift

--- Tests/SwiftExtensionsTests/URLTests.swift
+++ Tests/SwiftExtensionsTests/URLTests.swift
@@ -9,8 +9,23 @@ final class URLTests: XCTestCase {
         XCTAssertTrue(url.isXcodeProj)
     }
 
+    func testIsXcodeProjUpperCaseExtension() throws {
+        let url = try XCTUnwrap(URL(string: "test.XCODEPROJ"))
+        XCTAssertTrue(url.isXcodeProj)
+    }
+
     func testIsXcodeProjFalse() throws {
         let url = try XCTUnwrap(URL(string: "test.txt"))
+        XCTAssertFalse(url.isXcodeProj)
+    }
+
+    func testIsXcodeProjEmptyPathExtension() throws {
+        let url = try XCTUnwrap(URL(string: "test."))
+        XCTAssertFalse(url.isXcodeProj)
+    }
+    
+    func testIsXcodeProjNoExtension() throws {
+        let url = try XCTUnwrap(URL(string: "test"))
         XCTAssertFalse(url.isXcodeProj)
     }
 }
