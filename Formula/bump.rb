3c28537f62f7cd9b7a8769464c192513a9201cb0
Anka <runner@Mac-1726393097685.local>
2024-09-15 10:16:00 +0000

    Bump Formula to 2.0.0

--- Formula/bump.rb
+++ Formula/bump.rb
@@ -1,8 +1,8 @@
 class Bump < Formula
     desc "Command line tool for versioning your Xcode projects."
     homepage "https://github.com/homerooliveira/Bump"
-    url "https://github.com/homerooliveira/Bump.git", :tag => "1.1.0"
-    version "1.1.0"
+    url "https://github.com/homerooliveira/Bump.git", :tag => "2.0.0"
+    version "2.0.0"
     
     depends_on :xcode => ["11.0", :build]
     
