410bf7db6fb28d7071a26e9f00bf9af202d15150
Homero Oliveira <homero_oliveira@icloud.com>
2024-09-24 05:20:40 -0300

    Fix swiftlint

--- Tests/BumpCommandLineTests/BumpCommandTests.swift
+++ Tests/BumpCommandLineTests/BumpCommandTests.swift
@@ -33,10 +33,10 @@ final class BumpCommandTests {
     @Test func testBumpValidationErrorWhenBundleIdentifiersIsEmpty() throws {
         command.bundleIdentifiers = []
         command.mode = .build
-        
-        #expect { 
-            try command.validate() 
-        } throws: { error in 
+
+        #expect {
+            try command.validate()
+        } throws: { error in
             guard let error = error as? ValidationError else {
                 throw error
             }
