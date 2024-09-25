837e838e255aaff655ee4d8825c18d11a80069c7
Homero Oliveira <homero_oliveira@icloud.com>
2024-09-25 02:43:14 -0300

    Improve test code and removed mocks

--- Tests/FileManagerWrapperTests/URLTests.swift
+++ Tests/FileManagerWrapperTests/URLTests.swift
@@ -4,13 +4,14 @@ import Testing
 @testable import FileManagerWrapper
 
 struct URLTests {
-    @Test func isXcodeProjTrue() throws {
-        let url = try #require(URL(string: "test.xcodeproj"))
-        #expect(url.isXcodeProj)
-    }
-
-    @Test func isXcodeProjFalse() throws {
-        let url = try #require(URL(string: "test.txt"))
-        #expect(!url.isXcodeProj)
+    @Test(
+        arguments: [
+            (value: "test.xcodeproj", expected: true),
+            (value: "test.txt", expected: false)
+        ]
+    )
+    func isXcodeProj(value: String, expected: Bool) throws {
+        let url = try #require(URL(string: value))
+        try #require(url.isXcodeProj == expected)
     }
 }
